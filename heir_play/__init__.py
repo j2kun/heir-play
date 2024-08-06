5899d5417d8c03dbc6e7ded53b68f0e47abba604
Jeremy Kun <j2kun@users.noreply.github.com>
2024-08-06 16:51:08 -0700

    add heir-translate and refactor

--- heir_play/__init__.py
+++ heir_play/__init__.py
@@ -1,10 +1,15 @@
 """A magic for running heir-opt nightly binary"""
-__version__ = '0.0.1'
+__version__ = "0.0.1"
 
-from .heir_opt import load_nightly
 from .heir_opt import HeirOptMagic
+from .heir_opt import HeirTranslateMagic
+from .utils import load_nightly
+
 
 def load_ipython_extension(ipython):
-    binary_path = load_nightly()
-    magic = HeirOptMagic(ipython, binary_path=str(binary_path))
-    ipython.register_magics(magic)
+    ipython.register_magics(
+        HeirOptMagic(ipython, binary_path=str(load_nightly("heir-opt")))
+    )
+    ipython.register_magics(
+        HeirTranslateMagic(ipython, binary_path=str(load_nightly("heir-translate")))
+    )
--- heir_play/heir_opt.py
+++ heir_play/heir_opt.py
@@ -1,4 +1,3 @@
-from pathlib import Path
 import os
 import shlex
 import subprocess
@@ -8,52 +7,44 @@ from IPython.core.magic import cell_magic
 from IPython.core.magic import magics_class
 
 
-HEIR_OPT_URL = "https://github.com/google/heir/releases/download/nightly/heir-opt"
+class BinaryMagic(Magics):
+    def __init__(self, shell, binary_path):
+        """
+        Initialize a magic for running a binary with a given path.
+        """
+        super(BinaryMagic, self).__init__(shell)
+        self.binary_path = binary_path
 
+    def run_binary(self, line, cell):
+        """
+        Run the binary on the input cell.
 
-def abort_cleanup():
-    cwd = Path(dir=os.getcwd())
-    tmpfile = cwd / "heir-opt"
-    os.remove(tmpfile)
-
-
-def load_nightly() -> Path:
-    """Fetches the nightly heir-opt binary from GitHub and returns the path to it."""
-    print("Loading heir-opt nightly binary")
-    # TODO: how to clean up the tmpdir after ipython closes?
-    # At worst, the user will see this in their heir_play dir and delete it.
-    cwd = Path(dir=os.getcwd())
-    tmpfile = cwd / "heir-opt"
-    if os.path.isfile(tmpfile):
-        print("Using existing local heir-opt")
-        return tmpfile
-
-    # -L follows redirects, necessary for GH asset downloads
-    proc = subprocess.run(["curl", "-L", "-o", tmpfile, HEIR_OPT_URL])
-    if proc.returncode != 0:
-        print("Error downloading heir-opt")
-        print(proc.stderr)
-        return None
-
-    proc = subprocess.run(["chmod", "a+x", tmpfile])
-    if proc.returncode != 0:
-        print("Error modifying permissions on heir-opt")
-        print(proc.stderr)
-        abort_cleanup()
-        return None
-
-    return tmpfile
+        Args:
+            line: The options to pass to the binary.
+            cell: The input to pass to the binary.
+        """
+        print(f"Running {self.binary_path}...")
+        completed_process = subprocess.run(
+            [os.path.abspath(self.binary_path)] + shlex.split(line),
+            input=cell,
+            text=True,
+        )
+        if completed_process.returncode != 0:
+            print(f"Error running {self.binary_path}")
+            print(completed_process.stderr)
+            return
+        output = completed_process.stdout
+        print(output)
 
 
 @magics_class
-class HeirOptMagic(Magics):
+class HeirOptMagic(BinaryMagic):
     def __init__(self, shell, binary_path="heir-opt"):
         """
         Initialize heir-opt with a path to the heir-opt binary.
         If not specified, will assume heir-opt is on the path.
         """
-        super(HeirOptMagic, self).__init__(shell)
-        self.binary_path = binary_path
+        super(HeirOptMagic, self).__init__(shell, binary_path)
 
     @cell_magic
     def heir_opt(self, line, cell):
@@ -64,15 +55,25 @@ class HeirOptMagic(Magics):
             line: The options to pass to heir-opt.
             cell: The input to pass to heir-opt.
         """
-        print("Running heir-opt...")
-        completed_process = subprocess.run(
-            [os.path.abspath(self.binary_path)] + shlex.split(line),
-            input=cell,
-            text=True,
-        )
-        if completed_process.returncode != 0:
-            print("Error running heir-opt")
-            print(completed_process.stderr)
-            return
-        output = completed_process.stdout
-        print(output)
+        return self.run_binary(line, cell)
+
+
+@magics_class
+class HeirTranslateMagic(BinaryMagic):
+    def __init__(self, shell, binary_path="heir-translate"):
+        """
+        Initialize heir-translate with a path to the heir-translate binary.
+        If not specified, will assume heir-translate is on the path.
+        """
+        super(HeirTranslateMagic, self).__init__(shell, binary_path)
+
+    @cell_magic
+    def heir_translate(self, line, cell):
+        """
+        Run heir-translate on the input cell.
+
+        Args:
+            line: The options to pass to heir-translate.
+            cell: The input to pass to heir-translate.
+        """
+        return self.run_binary(line, cell)
